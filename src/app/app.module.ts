5a072e0359b0ae30de79797a5af444cce1931ca4
Aishwant <aishwantg@gmail.com>
2018-10-03 16:11:05 -0500

    configure modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,80 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+// import { SocketIoModule, SocketIoConfig } from 'socket.io';
 import { MyApp } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { FIREBASE_CONFIG } from './app.firebase.config';
+
+
+//pages
 import { LoginPage } from '../pages/login/login';
 import { SignupPage } from '../pages/signup/signup';
 import { FeedPage } from '../pages/feed/feed';
+import { FoundPage } from '../pages/found/found';
+import { LostPage } from '../pages/lost/lost';
+import { AboutAppPage } from '../pages/about-app/about-app';
+import { ChatPage } from '../pages/chat/chat';
+import { ListchatPage } from '../pages/listchat/listchat';
+import { SettingsPage } from '../pages/settings/settings';
+import { ServicesAuth } from '../providers/services-auth/services-auth';
+import { AboutAppPageModule } from '../pages/about-app/about-app.module';
+import { LoginPageModule } from '../pages/login/login.module';
+import { SignupPageModule } from '../pages/signup/signup.module';
+import { FeedPageModule } from '../pages/feed/feed.module';
+import { FoundPageModule } from '../pages/found/found.module';
+import { LostPageModule } from '../pages/lost/lost.module';
+import { ChatPageModule } from '../pages/chat/chat.module';
+import { ListchatPageModule } from '../pages/listchat/listchat.module';
+import { SettingsPageModule } from '../pages/settings/settings.module';
+
+
+
+
+// let config: SocketIoConfig = {
+//   url: "",
+//   options: {}
+// }
 
 @NgModule({
   declarations: [
     MyApp,
-    LoginPage,
-    SignupPage,
-    FeedPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireAuthModule,
+    AboutAppPageModule,
+    LoginPageModule,
+    SignupPageModule,
+    FeedPageModule,
+    FoundPageModule,
+    LostPageModule,
+    ChatPageModule,
+    ListchatPageModule,
+    SettingsPageModule
+    // SocketIoModule.forRoot(config)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     LoginPage,
     SignupPage,
-    FeedPage
+    FeedPage,
+    FoundPage,
+    LostPage,
+    AboutAppPage,
+    ChatPage,
+    ListchatPage,
+    SettingsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ServicesAuth,
   ]
 })
 export class AppModule {}
