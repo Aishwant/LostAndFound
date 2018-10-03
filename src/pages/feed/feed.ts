0f0eb058f2266dfe49c3e75a0539e329ea08e992
Aishwant <aishwantg@gmail.com>
2018-10-03 15:00:07 -0500

    Display lost and found items

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angular';
 
 /**
  * Generated class for the FeedPage page.
@@ -15,12 +15,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FeedPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  item = 'Found Items';
 
+  items: any = {
+    "Found Items":[
+      {
+        itemName:'Computer',
+        itemLocation: 'Weir Hall',
+        itemDescription: 'A macbook pro 13',
+        itemPicture: 'assets/imgs/macbook.jpg'
+      },
+      {
+        itemName:'Book',
+        itemLocation: 'J.D. Williams',
+        itemDescription: 'Law VS Power',
+        itemPicture: 'assets/imgs/book.jpg'
+      }
+    ],
+    "Lost Items":[
+      {
+        itemName:'Charger',
+        itemLocation: 'Library',
+        itemDescription: ''
+      }
+    ]
+  };
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public menu: MenuController) {
+    this.menu.swipeEnable(true);
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad FeedPage');
+  itemName(type:any){
+    return this.items[type];
   }
 
+  itemLocation(){
+    return this.items.itemLocation;
+  }
 }
