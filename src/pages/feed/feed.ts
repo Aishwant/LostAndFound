57bab1d1d3e66acf4573f12f3d7791514170227f
Aishwant <aishwantg@gmail.com>
2018-10-03 18:52:22 -0500

    More dynamic

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angular';
-
-/**
- * Generated class for the FeedPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { ItemDetailsPage } from '../item-details/item-details';
 
 @IonicPage()
 @Component({
@@ -52,4 +46,9 @@ export class FeedPage {
   itemLocation(){
     return this.items.itemLocation;
   }
+
+  getDetails(itemN: any){
+    this.navCtrl.push(ItemDetailsPage,{item: itemN});
+  }
+
 }
