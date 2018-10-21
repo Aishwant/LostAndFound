fe7a3e6d1fd47c9e40d3a1ef96961f7b9c8944d9
Aishwant <aishwantg@gmail.com>
2018-10-21 02:58:27 -0500

    Lost items now can be add to the database

--- src/pages/lost/lost.ts
+++ src/pages/lost/lost.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Items } from '../../models/item_interface';
+import { ItemService } from '../../providers/item-service/Item-service';
+import { FeedPage } from '../feed/feed';
 
 /**
  * Generated class for the LostPage page.
@@ -15,11 +18,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LostPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  items = {} as Items;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private iServ: ItemService) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LostPage');
+  newLost(items: Items){
+    this.iServ.createItem(items,"Lost");
+    this.navCtrl.setRoot(FeedPage);
   }
+
+  // ionViewDidLoad() {
+  //   console.log('ionViewDidLoad LostPage');
+  // }
 
 }
