632a243f807dc00f81af300e3d5d4d830b0f83a1
Aishwant <aishwantg@gmail.com>
2018-10-21 02:59:00 -0500

    Found items now can be add to the database

--- src/pages/found/found.ts
+++ src/pages/found/found.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Items } from '../../models/item_interface';
+import { ItemService } from '../../providers/item-service/Item-service';
+import { FeedPage } from '../feed/feed';
+
 /**
  * Generated class for the FoundPage page.
  *
@@ -15,11 +19,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FoundPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  items= {} as Items;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private iServ: ItemService) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad FoundPage');
+  newFound(items: Items){
+    this.iServ.createItem(items,"Found");
+    this.navCtrl.setRoot(FeedPage);
   }
+
+
+  // ionViewDidLoad() {
+  //   console.log('ionViewDidLoad FoundPage');
+  // }
 
 }
