6e430e6e3b62fb6972fa5665f943e5f340d65322
Aishwant <aishwantg@gmail.com>
2018-10-27 22:48:06 -0500

    Successfully created persistant login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -16,18 +16,25 @@ import { AngularFireDatabase } from 'angularfire2/database';
 export class LoginPage {
 
   user = {} as User;
-
+  toggleState=false;
   constructor(public navCtrl: NavController, public alertCtrl: AlertController, public menu: MenuController, private sAuth: ServicesAuth, private db: AngularFireDatabase) {
     this.menu.swipeEnable(false);
+    console.log(this.toggleState);
   }
 
   login(user: User){
     if(/^[a-zA-Z0-9_@.]+$/.test(user.email) && user.password){ //validity check
       this.sAuth.loginVerificationEmail(user)
       .then(
-        () =>
-        this.navCtrl.setRoot(FeedPage),
-        error =>{
+        () =>{
+        if(this.toggleState){
+          localStorage.setItem("rememberMe",this.toggleState+"");
+          localStorage.setItem("email",this.user.email);
+          localStorage.setItem("password",this.user.password);
+          console.log("reached")
+        }
+        this.navCtrl.setRoot(FeedPage)
+        },error =>{
           const alert = this.alertCtrl.create({
           title: "Invalid",
           subTitle: "The Email or Password is incorrect",
@@ -52,5 +59,18 @@ export class LoginPage {
 
   redirectTo_forgotpwd(){
     this.navCtrl.push(ForgotpwdPage);
+  }
+
+  stateChangeToggle(){
+    if(this.toggleState) this.toggleState=false;
+    else this.toggleState=true;
+  }
+
+  ionViewDidEnter(){
+    if(localStorage.getItem("rememberMe")){
+      this.user.email=localStorage.getItem("email");
+      this.user.password=localStorage.getItem("password");
+      this.login(this.user);
+    }
   }
 }
